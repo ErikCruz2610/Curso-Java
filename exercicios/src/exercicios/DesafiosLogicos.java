800594db433013f3eca71936777d98132c420ed0
Erik Cesar Costa Da Cruz <75645020+ErikCruz2610@users.noreply.github.com>
2021-06-22 21:00:49 -0300

    novo projeto

--- exercicios/src/exercicios/DesafiosLogicos.java
+++ exercicios/src/exercicios/DesafiosLogicos.java
@@ -1,6 +1,15 @@
 package exercicios;
 
 public class DesafiosLogicos {
+	public static void main(String[] args) {
+		
+		boolean trabalho1 = true;
+		boolean trabalho2 = true;
+		
+		boolean comprouTV50 = trabalho1 && trabalho2;
+		
+		System.out.println("Comprou TV 50\"?" + comprouTV50);
+	}
 	
 	
 
